e569eb317587ec40c256b05aaeed96edda0b34aa
yoda <Siamak>
2021-01-06 10:52:47 +0330

    commit after adding plot tree to xgb

--- supervised_algo/XGB_Classifier_decision_tree.py
+++ supervised_algo/XGB_Classifier_decision_tree.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
 import time
 from matplotlib import dates as mpl_dates
 import sklearn
@@ -22,6 +23,9 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import cross_validate
 from sklearn.neighbors import KNeighborsClassifier
+# import os
+# os.environ["PATH"] += os.pathsep + 'C:\Program Files\Graphviz\bin'
+import sys
 import prepossessed_dataset
 import evaluator
 from sklearn.metrics import roc_curve
@@ -29,7 +33,8 @@ import seaborn as sns
 import xgboost as xgb
 from sklearn import metrics
 from xgboost import XGBClassifier
-
+sys.path.insert(0, "D:\\Graphviz\\bin")
+sys.path.insert(0, "D:\\Graphviz")
 sns.set()
 
 dataset = prepossessed_dataset.labeled()
@@ -41,8 +46,29 @@ x_cv = dataset["x_cv"]
 y_cv = dataset["y_cv"]
 
 clf = XGBClassifier()
+# clf = xgb.sklearn.XGBClassifier(nthread=-1, seed=42)
 clf.fit(x_train, y_train)
-# xgb_pred = clf.predict(x_train)
+xgb_pred = clf.predict(x_train)
 evaluator.evaluate_preds(clf, x_train, y_train, x_test, y_test, x_cv, y_cv)
-# plt.figure(figsize=(20, 15))
-# xgb.plot_tree(clf, ax=plt.gca())
+plt.figure(figsize=(20, 15))
+xgb.plot_importance(clf, ax=plt.gca())
+plt.show()
+plt.figure(figsize=(20,15))
+xgb.plot_tree(clf, ax=plt.gca())
+plt.show()
+print("Number of boosting trees: {}".format(clf.n_estimators))
+print("Max depth of trees: {}".format(clf.max_depth))
+print("Objective function: {}".format(clf.objective))
+file_name = "xgb.pkl"
+# save
+pickle.dump(clf, open(file_name, "wb"))
+pickle_file = open("file_name", "rb")
+objects = []
+while True:
+    try:
+        objects.append(pickle.load(pickle_file))
+    except EOFError:
+        break
+pickle_file.close()
+
+print(objects)
